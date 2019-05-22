905f02001fc4ac6d6f5a25d166f2e09036590de0
cC <>
2019-05-23 00:29:33 +0800

    by cc

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -463,10 +463,14 @@ module.exports = function(webpackEnv) {
               // its runtime that would otherwise be processed through "file" loader.
               // Also exclude `html` and `json` extensions so they get processed
               // by webpacks internal loaders.
-              exclude: [/\.(js|mjs|jsx|ts|tsx)$/, /\.html$/, /\.json$/],
+              exclude: [/\.(js|mjs|jsx|ts|tsx)$/, /\.html$/, /\.json$/, /.scss$/],
               options: {
                 name: 'static/media/[name].[hash:8].[ext]',
               },
+            },
+            {
+                test:/\.scss$/,
+                loaders:['style-loader','css-loader','sass-loader'],
             },
             // ** STOP ** Are you adding a new loader?
             // Make sure to add the new loader(s) before the "file" loader.
